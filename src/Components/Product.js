1c3c01e87392db5e801e25f536e452e6b2432aeb
Salman-Raza-stack <salmanabid301@gmail.com>
2020-07-08 03:04:35 +0500

    shoe store completed

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,18 +1,20 @@
 import React from "react";
 import Shoes from "./../shoes.json";
+import { Link } from "react-router-dom";
 
 function Product() {
   return (
     <div>
-      <h1>Welcome to Product</h1>
-      <div>
+      <h1 className="welcome">Welcome to My Products</h1>
+      <div className="productContainer">
         {Object.keys(Shoes).map((keyName) => {
           const shoe = Shoes[keyName];
           return (
-            <div key={keyName}>
-              <h2>{shoe.name}</h2>
+            <Link key={keyName} className="link" to={`/product/${keyName}`}>
+              <h4 className="productname">{shoe.name}</h4>
+              <hr />
               <img src={shoe.img} height={150} />
-            </div>
+            </Link>
           );
         })}
       </div>
--- src/Components/ProductItmes.js
+++ src/Components/ProductItmes.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import Shoes from "./../shoes.json";
 
 function ProductItems() {
+  const { id } = useParams();
+  const shoe = Shoes[id];
+  if (!shoe) return <h2>Product not Found</h2>;
   return (
     <div>
       <h1>Welcome to ProductItems</h1>
+      <div className="link">
+        <h4 className="productname">{shoe.name}</h4>
+        <hr />
+        <img src={shoe.img} height={500} />
+      </div>
     </div>
   );
 }
